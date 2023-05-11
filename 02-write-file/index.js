8ee3b5f3c3456d13fd00e58206913d413aaa4bfa
sergokok <voronokins@yandex.ru>
2023-05-11 20:45:13 +0300

    fix # 2 and 4

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,58 +1,42 @@
-//
-// // импортируем модуль fs
-// const fs = require('fs');
-//
-// // приветствие
-// console.log("ПРИВЕТ! Введите текст:");
-//
-// // поток для чтения ввода в терминале
-// const stdin = process.openStdin();
-//
-// // чтение ввода и дописывание в файл новго текста
-// stdin.addListener('data', function(user_input) {
-//   // чтение содержимого файла
-//   fs.readFile('02-write-file/out-text.txt', 'utf8', function(err, data) {
-//     if(err) {
-//       console.log(err);
-//     } else {
-//       // дополнение в файл нового текста и запись
-//       const updated_data = data + "\n" + user_input.toString().trim();
-//       fs.writeFile('02-write-file/out-text.txt', updated_data, function(err) {
-//         if(err) {
-//           console.log(err);
-//         } else {
-//           console.log("Новый текст был успешно дополнен в файл out-text.txt");
-//           console.log("Введите еще текст:");
-//         }
-//       });
-//     }
-//   });
-//   // обработка exit
-//   if(user_input.toString().trim() === 'exit') {
-//     console.log("Процесс завершен.");
-//     process.exit();
-//   }
-// });
-// // при нажатии Ctrl + C
-// process.on('SIGINT', function() {
-//   console.log("\nПроцесс завершен.");
-//   process.exit();
-//
-// });
+
+// импортируем модуль fs
 const fs = require('fs');
+
+// приветствие
 console.log("ПРИВЕТ! Введите текст:");
+
+// поток для чтения ввода в терминале
 const stdin = process.openStdin();
+
+// чтение ввода и дописывание в файл новго текста
 stdin.addListener('data', function(user_input) {
-  fs.writeFile('02-write-file/out-text.txt', user_input.toString().trim(), function(err) {
+  // чтение содержимого файла
+  fs.readFile('02-write-file/out-text.txt', 'utf8', function(err, data) {
     if(err) {
       console.log(err);
     } else {
-      console.log("Новый текст был успешно записан в файл out-text.txt");
-      console.log("Введите еще текст:");
+      // дополнение в файл нового текста и запись
+      const updated_data = data + "\n" + user_input.toString().trim();
+      fs.writeFile('02-write-file/out-text.txt', updated_data, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Новый текст был успешно дополнен в файл out-text.txt");
+          console.log("Введите еще текст:");
+        }
+      });
     }
   });
+  // обработка exit
+  if(user_input.toString().trim() === 'exit') {
+    console.log("Процесс завершен.");
+    process.exit();
+  }
 });
+// при нажатии Ctrl + C
 process.on('SIGINT', function() {
   console.log("\nПроцесс завершен.");
   process.exit();
+
 });
+
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,42 +1,18 @@
-// // подключаем fs и path
-// const fs = require('fs');
-// const path = require('path');
-//
-// const copyDirectory = (source, destination) => {
-//   fs.mkdirSync(destination, {recursive: true}); // создаем директорию
-//
-//   const entries = fs.readdirSync(source, {withFileTypes: true}); // Получаем массив
-//
-//   for (let entry of entries) {
-//     const sourcePath = path.join(source, entry.name);
-//     const destinationPath = path.join(destination, entry.name); // Итерируемся. Путь оттуда сюда.
-//
-//     if (entry.isDirectory()) {
-//       copyDirectory(sourcePath, destinationPath);
-//     } else {
-//       fs.copyFileSync(sourcePath, destinationPath);
-//     } // если директория и файл то копу=ируем в новое место
-//   }
-// };
-//
-// const source = path.join(__dirname, 'files'); // путь к исходному
-// const destination = path.join(__dirname, 'files-copy'); // папка копия
-//
-// copyDirectory(source, destination);
-
-//ИСПРАВИЛСЯ
+//  fs и path
 const fs = require('fs').promises;
 const path = require('path');
 
+// рекурсивно копируем
 const copyDirectory = async (source, destination) => {
-  await fs.mkdir(destination, {recursive: true});
-
-  const entries = await fs.readdir(source, {withFileTypes: true});
-
+  // удаляем папку копии если есть такая
+  await fs.rm(destination, { recursive: true, force: true });
+  // дальше создаем получаем список что в ней есть проходим по списку  составляем пути
+  await fs.mkdir(destination, { recursive: true });
+  const entries = await fs.readdir(source, { withFileTypes: true });
   for (let entry of entries) {
     const sourcePath = path.join(source, entry.name);
     const destinationPath = path.join(destination, entry.name);
-
+    // +на всякий если папка+ то рекопим папки а иначе файлы
     if (entry.isDirectory()) {
       await copyDirectory(sourcePath, destinationPath);
     } else {
